639bcafba925220737542f9229f008f1b02c5f39
riamems <membagloria@gmail.com>
2023-03-20 14:38:45 -0700

    7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main -main function
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int s;
+	char m;
+
+	for (s = 48; s = 58; s++)
+		putchar(s);
+	for (m = 'a'; m <= 'f'; m++)
+		putchar(m);
+	return (0);
+}

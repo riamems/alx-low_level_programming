282ad3f3b38cd06365701fd67c34ae0d22187f69
riamems <membagloria@gmail.com>
2023-03-20 13:58:31 -0700

    capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main -main function
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'A'; d <= 'Z'' d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}

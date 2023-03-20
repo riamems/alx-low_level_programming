924cff762c39b0b01b12345bb6132125eb8ecdaf
riamems <membagloria@gmail.com>
2023-03-20 14:13:01 -0700

    5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main -main function
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", k);
+	}
+	printf("\n");
+	return (0);
+}

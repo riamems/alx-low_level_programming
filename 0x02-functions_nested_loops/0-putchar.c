c03ae13c44701dc7c88f99339448af5ee79e7595
riamems <membagloria@gmail.com>
2023-03-22 19:35:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/*
-*_putchar- prints the alphabet
- *
- *Return: always 0
- *
- */
+/**
+*putchar- prints the alphabet and a new line
+*Return: always 0
+*
+*/
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putcahr('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');

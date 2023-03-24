4b52ac5d236fa9a6d5c0f2c107eda957a3a4ba91
riamems <membagloria@gmail.com>
2023-03-24 08:04:32 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isuper -checks if a letter is uppercase
+ *
+ *@b: the number to be checked
+ *
+ * Return: 1 if c is uppercase 0 if otherwise
+ */
+
+int _isupper(int b);
+
+{
+int b;
+
+if (b > 65 && b <= 90)
+{
+return (1);
+}
+return (0);
+}

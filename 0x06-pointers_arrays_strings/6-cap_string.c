e8c31d3202c6ea119702190d2cd7a09771f81fe1
riamems <membagloria@gmail.com>
2023-03-30 11:32:47 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * cap_string -capitalizes words of a string
+ * cap_string -capitalizes all words of a string
  *@str: string
  *Return: pointer to a string
  */
 char *cap_string(char *str)
-
 {
 	int i = 0;
 

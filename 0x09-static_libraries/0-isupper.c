a5f543bc4399508230255d8870dcca000bc17b14
riamems <membagloria@gmail.com>
2023-04-08 07:28:36 -0700

    Updated

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper -checks if a letter is uppercase
+ *
+ *@b: the number to be checked
+ *
+ * Return: 1 if b is uppercase 0 if otherwise
+ */
+
+int _isupper(int b)
+
+{
+
+if (b > 65 && b <= 90)
+{
+return (1);
+}
+return (0);
+}
+

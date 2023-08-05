7d90161e480ad13b32e81aef230a45b225ed2865
riamems <membagloria@gmail.com>
2023-08-05 09:18:14 -0700

    static library for object files

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area
+ * @src: memory to copy from
+ * @n: bytes to be copied from
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+int j = n;
+
+for (; i < j; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}

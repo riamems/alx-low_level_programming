c40aa1768303aba589f28296ec17b78d8496bed8
riamems <membagloria@gmail.com>
2023-08-25 02:27:12 -0700

    linked c work

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * printLines - prints a sentence before the main function is executed
+ */
+void printLines(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}

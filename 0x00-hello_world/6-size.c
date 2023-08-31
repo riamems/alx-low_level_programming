e76d4cdbc925295b845cb30ac3794926b851bcd8
riamems <membagloria@gmail.com>
2023-08-31 07:51:49 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,20 +6,15 @@
 
 int main(void)
 {
-int a = 5;
-char i = 8 ;
-
-printf("char\t\t%d\n", sizeof(char));
-printf("short\t\t%d\n", sizeof(short));
-printf("int\t\t%d\n", sizeof(int));
-printf("long\t\t%d\n", sizeof(long));
-printf("long long\t%d\n", sizeof(long long));
-printf("float\t\t%d\n", sizeof(float));
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of float: %lu  byte(s)\n", sizeof(float));
-
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

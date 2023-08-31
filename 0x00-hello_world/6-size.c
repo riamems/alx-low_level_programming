78636656852deb169314cbea8b2fd06db679e29b
riamems <membagloria@gmail.com>
2023-08-31 07:46:11 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,14 @@
 int main(void)
 {
 int a = 5;
-char i = 8;
+char i = 8 ;
+
+printf("char\t\t%d\n", sizeof(char));
+printf("short\t\t%d\n", sizeof(short));
+printf("int\t\t%d\n", sizeof(int));
+printf("long\t\t%d\n", sizeof(long));
+printf("long long\t%d\n", sizeof(long long));
+printf("float\t\t%d\n", sizeof(float));
 printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long int: %lu byte(s)\n", sizeof(long int));

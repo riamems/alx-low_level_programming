4d7d75f99ab8037df5386280a0d2a0a546cead6b
riamems <membagloria@gmail.com>
2023-09-18 23:09:11 -0700

    nested loops and fuucntions work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,11 @@
 #include "main.h"
 /**
-*putchar- prints the alphabet and a new line
+ * main - prints the alphabet and a new line
 *Return: always 0
 *
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+printf ("_putchar\n");
+return (0);
 }

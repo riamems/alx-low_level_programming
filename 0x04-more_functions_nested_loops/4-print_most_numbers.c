40f7474bdee08d11d1e8e1a81f249b24c0555e43
riamems <membagloria@gmail.com>
2023-10-07 02:04:50 -0700

    nested loops work

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,15 @@
 /**
  *print_most_numbers - prints the numbers from 0 to 9
  *
- * Return: printed numbers from 0 to 9 except 2 and 4
+ * Return: void
  */
 void print_most_numbers(void)
 {
-int k;
+char k;
 
 for (k = '0'; k <= '9'; k++)
 {
-if (k != 2 && k != 4)
+if (!(k == '2' || k == '4'))
 
 _putchar(k);
 }

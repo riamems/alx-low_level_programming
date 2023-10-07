d162e98484d1261a16709cc757fa7efab986c695
riamems <membagloria@gmail.com>
2023-10-07 02:18:19 -0700

    nested loops work

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,18 @@
  */
 void more_numbers(void)
 {
-	int a;
-	int b;
+int a;
+int b;
 
-	for (a = 0; a <= 10; a++)
-	{
-		for (b = 0; b <= 10; b++)
-		{
-			if (a > 9)
-			{
-				_putchar((b / 10) + '0');
-			}
-			_putchar((b % 10) + '0');
-		}
-		_putchar('\n');
-	}
+for (a = 1; a <= 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 10)
+_putchar('1');
+
+_putchar((b % 10) + '0');
+}
+_putchar('\n');
+}
 }

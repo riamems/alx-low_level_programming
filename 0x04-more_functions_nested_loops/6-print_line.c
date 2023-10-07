aa760ed8b1e3b17b4ec6bd69b7b75339d3bf2705
riamems <membagloria@gmail.com>
2023-10-07 02:47:19 -0700

    nested loops work

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_line -draws a straight line according to parameter
+ * print_line - draws a straight line according to parameter
  * @n: the number of lines to draw
- * Return: empty
+ * Return: void
  */
 void print_line(int n)
 {
@@ -13,9 +13,9 @@ _putchar('\n');
 }
 else
 {
-for (p = 0; n < p; p++)
+for (p = 1; p <= n; p++)
 {
-_putchar(95);
+_putchar('_');
 }
 _putchar('\n');
 

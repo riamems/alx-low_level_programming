406419fc6dc924227b3adc374e09f84088351748
riamems <membagloria@gmail.com>
2023-11-09 02:11:47 -0800

    double linked lists work

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,20 +2,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a dlistint_t list
- * @h: Pointer to the header of the list
- * Return: Number of nodes in the list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes.
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t node_count = 0;
+size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		node_count++;
-	}
-	return (node_count);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+return (count);
 }

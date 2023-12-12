10f49d2180a7a24149fa193a01e90b2ad74a5fa9
riamems <membagloria@gmail.com>
2023-12-12 01:45:38 -0800

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,18 @@
  */
 void print_rev(char *s)
 {
-	int a = 0;
+int a = 0;
 
-		while (s[a] != '\0')
-		{
-			a++;
-		}
-		s++;
-		for (a -= 1; a >= 0;)
-		{
-			_putchar(s[a]);
-		}
-		_putchar('\n');
+while (s[a] != '\0')
+{
+a++;
+}
+s++;
+for (a -= 1; a >= 0;)
+{
+_putchar(s[a]);
+}
+_putchar('\n');
 }
 
 
